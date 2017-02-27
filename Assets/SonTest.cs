3b963496b009b9c0ed89ac60c50edd54f3715082
Maxilag <guillaume.seyer@edu.itescia.fr>
2017-02-27 13:50:44 +0100

    SonPrincipaux

---
+++ Assets/SonTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SonTest : MonoBehaviour {
+
+	FMOD.Studio.EventInstance myEvent;
+	FMOD.Studio.ParameterInstance myParamater;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
