2389cc60e76d752a660df4c17c4d45d125fd4d3c
yann <yannpassy1@gmail.com>
2017-03-22 14:37:32 +0100

    Revert "revert"
    This reverts commit 7a9216bc4c2b567342eddad5fcae3e2f88382690.

--- Assets/MyScript/Initiation.cs
+++ Assets/MyScript/Initiation.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class Initiation : MonoBehaviour {
 	public Transform objectReference;
 	public OVRCameraRig cameraOVR;
-	private enum Etat {texte1, texte2, texte3, texte4, tp, texte5, demiTour, texte6, destructionFragment, cristauxPowers, fadeOut, fadeIn };
+	private enum Etat {texte1, texte2, texte3, texte4, tp, texte5, demiTour, texte6, destructionFragment, cristauxPowers};
 	Etat etat;
 
-    private Vector3 centreCamera;
+	private Vector3 centreCamera;
 	private Vector3 nouvellePosition;
 
 	private double chrono;
@@ -40,15 +39,10 @@ public class Initiation : MonoBehaviour {
 	private Animation anim;
 	private Animation animPioche;
 
-    Tween tweenPioche;
-    private bool destructionCristaux;
-    private double chronoFadeOut;
-    private double chronoFadeIn;
-
-    // Use this for initialization
-    void Start () {
-        destructionCristaux = false;
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -64,97 +58,6 @@ public class Initiation : MonoBehaviour {
 
 		}
 
-        animPioche["pioche anim"].speed = 6.0f;
 
-        if (etat == Etat.cristauxPowers)
-        {
-            cristauxPowers.transform.GetChild(0).gameObject.transform.GetComponent<Rigidbody>().isKinematic = false;
-            tweenPioche = pioche.transform.DOMove(cristauxPowers.transform.position, 0.35f);
-            StartCoroutine(Pioche());
-            destructionCristaux = true;
-            cristauxPowers.GetComponent<MeshCollider>().enabled = false;
-            chrono = 0;
-            etat = Etat.texte4;
-        }
-        else if (etat == Etat.fadeOut)
-        {
-            chronoFadeOut += Time.deltaTime;
-            if (chronoFadeOut > cam.GetComponent<OVRScreenFadeOut>().fadeTime)
-            {
-                etat = Etat.tp;
-                chronoFadeOut = 0;
-            }
-        }
-        else if (etat == Etat.tp)
-        {
-            this.transform.position = new Vector3(nouvellePosition.x, nouvellePosition.y + 0.066f, nouvellePosition.z);
-            FMODUnity.RuntimeManager.PlayOneShot("event:/instant-teleport", this.transform.position);
-            cam.GetComponent<OVRScreenFadeOut>().StartFadeIn();
-
-            etat = Etat.fadeIn;
-        }
-        else if (etat == Etat.fadeIn)
-        {
-            curseur.transform.GetChild(0).GetComponent<Animation>().Stop();
-            chronoFadeIn += Time.deltaTime;
-            if (chronoFadeIn > cam.GetComponent<OVRScreenFadeOut>().fadeTime)
-            {
-                //on part sur un renderer noir pour le curseur
-                curseur.transform.GetChild(0).gameObject.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.black;
-                curseur.transform.GetChild(0).gameObject.transform.GetChild(1).GetComponent<Renderer>().material.color = Color.black;
-                curseur.transform.GetChild(0).gameObject.transform.GetChild(2).GetComponent<Renderer>().material.color = Color.black;
-                curseur.transform.GetChild(0).gameObject.transform.GetChild(3).GetComponent<Renderer>().material.color = Color.black;
-                curseur.transform.GetChild(0).gameObject.transform.GetChild(4).GetComponent<Renderer>().material.color = Color.black;
-                chronoFadeIn = 0;
-                chrono = 0;
-                cam.GetComponent<OVRScreenFadeOut>().enabled = false;
-                etat = Etat.texte5;
-            }
-        }
-        else if (etat == Etat.demiTour)
-        {
-            cam.GetComponent<OVRScreenFadeOut>().enabled = true;
-            cam.GetComponent<OVRScreenFadeOut>().StarFadeOut();
-            chronoFadeOut += Time.deltaTime;
-            if (chronoFadeOut > cam.GetComponent<OVRScreenFadeOut>().fadeTime)
-            {
-                chronoFadeOut = 0;
-                FMODUnity.RuntimeManager.PlayOneShot("event:/instant-teleport", this.transform.position);
-                this.transform.rotation *= Quaternion.AngleAxis(180, Vector3.up);
-                cam.GetComponent<OVRScreenFadeOut>().StartFadeIn();
-                // fade in
-                curseur.transform.GetChild(0).GetComponent<Animation>().Stop();
-                chronoFadeIn += Time.deltaTime;
-                if (chronoFadeIn > cam.GetComponent<OVRScreenFadeOut>().fadeTime)
-                {
-                    curseur.transform.GetChild(0).gameObject.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.black;
-                    curseur.transform.GetChild(0).gameObject.transform.GetChild(1).GetComponent<Renderer>().material.color = Color.black;
-                    curseur.transform.GetChild(0).gameObject.transform.GetChild(2).GetComponent<Renderer>().material.color = Color.black;
-                    curseur.transform.GetChild(0).gameObject.transform.GetChild(3).GetComponent<Renderer>().material.color = Color.black;
-                    curseur.transform.GetChild(0).gameObject.transform.GetChild(4).GetComponent<Renderer>().material.color = Color.black;
-                    chronoFadeIn = 0;
-                    chrono = 0;
-                    cam.GetComponent<OVRScreenFadeOut>().enabled = false;
-                    etat = Etat.texte6;
-                }
-            }
-
-        } else if (etat == Etat.destructionFragment)
-        {
-
-        }
-    }
-
-    IEnumerator Pioche()
-    {
-        yield return new WaitForSeconds(0.55f);
-        //pioche.transform.position = cam.transform.position + cam.transform.rotation * Vector3.forward * 2.0f;
-        pioche.SetActive(false);
-    }
-
-    IEnumerator FinDestructionCristaux()
-    {
-        yield return new WaitForSeconds(0.55f);
-        destructionCristaux = false;
-    }
+	}
 }
