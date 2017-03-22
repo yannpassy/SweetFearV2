770ce00500072a5e733b0bb0383262f5c6ed4e58
Maxilag <guillaume.seyer@edu.itescia.fr>
2017-03-22 12:06:36 +0100

    script initiation

--- Assets/MyScript/Initiation.cs
+++ Assets/MyScript/Initiation.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Initiation : MonoBehaviour {
 	public Transform objectReference;
 	public OVRCameraRig cameraOVR;
+	public TextMeshProUGUI tmp;
 	private enum Etat {texte1, texte2, texte3, texte4, tp, texte5, demiTour, texte6, destructionFragment };
 	Etat etat;
 
 	private Vector3 centreCamera;
 	private Vector3 nouvellePosition;
 
-	private double chrono;
+	private float chronoFadeIn;
+	private float chronoFadeOut;
+	private float chronoValidation;
+	private float chronoValidationOld;
+	private bool passage;
 
 	private string tagTouchee;
 
@@ -39,8 +45,13 @@ public class Initiation : MonoBehaviour {
 	private Animation anim;
 	private Animation animPioche;
 
+	private Color myColor;
+	private float duration;
+	private float ratio;
 	// Use this for initialization
 	void Start () {
+		etat = 0;
+		duration = 4;
 		
 	}
 	
@@ -58,6 +69,70 @@ public class Initiation : MonoBehaviour {
 
 		}
 
+		if (etat == Etat.texte1) {
+			tmp.text = "Bienvenue dans SWEETFEAR";
+			if (passage == false) {
+				FadeInText ();
+			}
+
+			if (tagTouchee == "Canvas" && chronoFadeIn > duration) {
+				chronoValidationOld = chronoValidation;
+				chronoValidation += Time.deltaTime;
+				if (chronoValidationOld < 4 && chronoValidation >= 4) {
+					Debug.Log ("on arrive a 4");
+					chronoValidation = 0;
+					chronoValidationOld = 0;
+					passage = true;
+				}
+			}
+
+			if (passage == true) {
+				FadeOutText ();
+			}
+		}
+
+		if (etat == Etat.texte2) {
+			tmp.text = "Pouvez-vous vous en sortir?";
+			if (passage == false) {
+				FadeInText ();
+			}
+
+			if (tagTouchee == "Canvas" && chronoFadeIn > duration) {
+				chronoValidationOld = chronoValidation;
+				chronoValidation += Time.deltaTime;
+				if (chronoValidationOld < 4 && chronoValidation >= 4) {
+					chronoValidation = 0;
+					chronoValidationOld = 0;
+					passage = true;
+				}
+			}
+
+			if (passage == true) {
+				FadeOutText ();
+			}
+		}
+
 
 	}
+	void FadeInText(){
+		chronoFadeIn += Time.deltaTime;
+		myColor = tmp.color;
+		float ratio = chronoFadeIn / duration;
+		myColor.a = Mathf.Lerp (0, 1, ratio);
+		tmp.color = myColor;
+	}
+
+	void FadeOutText(){
+		chronoFadeOut += Time.deltaTime;
+		myColor = tmp.color;
+		float ratio = chronoFadeOut / duration;
+		myColor.a = Mathf.Lerp (1, 0, ratio);
+		tmp.color = myColor;
+		if (chronoFadeOut > duration) {
+			chronoFadeIn 	= 0;
+			chronoFadeOut 	= 0;
+			passage = false;
+			etat += 1;
+		}
+	}	
 }
