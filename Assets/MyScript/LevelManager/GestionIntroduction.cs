23d4101a088507e925db655a845e288437e1b932
MaxilagBis <guillaumeseyer@hotmail.fr>
2017-06-01 16:37:08 +0200

    changement musique + intro

--- Assets/MyScript/LevelManager/GestionIntroduction.cs
+++ Assets/MyScript/LevelManager/GestionIntroduction.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 using DG.Tweening;
 
 public class GestionIntroduction : MonoBehaviour {
+	public string myAmbiance = "event:/Musique intro";
+	FMOD.Studio.EventInstance musiqueIntro;
+	FMOD.Studio.ParameterInstance parameterIntro;
 	public float chrono;
 	public float[] duration = new float[4];
+	public float[] transition = new float[4];
 	public GameObject[] lightScene = new GameObject[4];
 	public int compteur;
 	// Use this for initialization
 	void Start () {
+		musiqueIntro = FMODUnity.RuntimeManager.CreateInstance (myAmbiance);
+		musiqueIntro.start ();
+		musiqueIntro.getParameter ("transition", out parameterIntro);
 	}
 
 	// Update is called once per frame
@@ -18,6 +25,7 @@ public class GestionIntroduction : MonoBehaviour {
 
 		if (chrono >= duration[compteur] && compteur <= 4) {
 			lightScene [compteur].GetComponent<Light> ().DOIntensity (1.5f, 2.0f);
+			parameterIntro.setValue (transition[compteur]);
 			compteur++;
 		}
 
